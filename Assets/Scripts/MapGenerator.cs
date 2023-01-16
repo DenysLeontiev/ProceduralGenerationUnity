2ddac2ad9b309d6b72d38c2d04c8bfcca8708505
Denys Leon <leonden0046@gmail.com>
2023-01-16 16:26:20 +0200

    Better noise generation(Lesson 3)

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -8,14 +8,36 @@ public class MapGenerator : MonoBehaviour
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
+    public int seed;
+    public int octaves;
+    [Range(0, 1f)]
+    public float persistance;
+    public float lacunarity;
+    public Vector2 offset;
 
     public bool autoUpdate;
 
     public void GenerateMap()
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed ,noiseScale, octaves, persistance, lacunarity, offset);
 
         MapDisplay mapDisplay = FindObjectOfType<MapDisplay>();
         mapDisplay.DrawNoiseMap(noiseMap);
+    }
+
+    public void OnValidate()
+    {
+        if(mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if(mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+        if(lacunarity < 1f)
+        {
+            lacunarity = 1f;
+        }
     }
 }
