dc57ee31c5183faa7350a3b3e61abdcaac3af00a
Denys Leon <leonden0046@gmail.com>
2023-01-18 20:58:09 +0200

    Endless Terrain(Lesson 7)

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -8,7 +8,7 @@ public class MapGenerator : MonoBehaviour
     public enum DrawMode{ColourMap, HeightMap, DrawMesh}
     public DrawMode drawMode;
 
-    const int mapChunkSize = 241;
+    public const int mapChunkSize = 241;
     [Range(0,6)]
     public int levelOfDetail;
 
